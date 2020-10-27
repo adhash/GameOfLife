9dbdc13c2f721df1c22bf06332294fbfb19da82d
adhash <wojciech.piecha@gmail.com>
2020-10-27 09:27:22 +0100

    removed comments

--- GameOfLife/Configuration/Application.swift
+++ GameOfLife/Configuration/Application.swift
@@ -1,11 +1,3 @@
-//
-//  Application.swift
-//  GameOfLife
-//
-//  Created by wojtek on 27/07/2020.
-//  Copyright © 2020 wojtek. All rights reserved.
-//
-
 import Foundation
 
 enum Application: String {
