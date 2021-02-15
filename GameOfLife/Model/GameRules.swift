de873b3a31c44137c2323789fdbb077ab514e483
adhash <wojciech.piecha@gmail.com>
2021-02-15 12:13:05 +0100

    Update GameRules.swift

--- GameOfLife/Model/GameRules.swift
+++ GameOfLife/Model/GameRules.swift
@@ -1,11 +1,3 @@
-//
-//  GameRules.swift
-//  GameOfLife
-//
-//  Created by wojtek on 27/07/2020.
-//  Copyright © 2020 wojtek. All rights reserved.
-//
-
 import Foundation
 
 protocol GameRules {
